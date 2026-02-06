8e4f40caa5f976665e510e932805d6bfaa3b3a13
VannShiki <sanjayaarie858@gmail.com>
2026-02-06 15:02:00 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week01
+
+fun main(){
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + "Nilai: " + score)
+}
