00c32473022a8fb972d9588c23457f4897c30a96
VannShiki <sanjayaarie858@gmail.com>
2026-02-20 15:24:09 +0700

    week03: cleanup code

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week03/Main.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_00000119085_IvanMichaelLawrenceSanjaya.week03
 fun main(){
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print error
+    e.salary = -1000 // Balik ke 0
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5 // Kalo di uncomment, bakal merah (Error)
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
