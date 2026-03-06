08a954f3bff3c2cf49f5830fe6f957be39522fff
VannShiki <sanjayaarie858@gmail.com>
2026-03-06 14:41:47 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week05/MathHelper.kt
@@ -0,0 +1,16 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week05
+
+class MathHelper() {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
