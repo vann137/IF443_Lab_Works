8303b36d3391effe02be5edc1c4f6792f92590a0
VannShiki <sanjayaarie858@gmail.com>
2026-03-27 11:34:33 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week07/Main.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week07/Main.kt
@@ -51,4 +51,19 @@ fun main (){
 
     val starterWeapon = Weapon.forgeStarterSword()
     println("Starter Weapon: $starterWeapon")
+
+    println("")
+
+    // Ceritanya pemain pergi ke Blacksmith (Pandai Besi) untuk upgrade senjata wow
+    val upgradedStarterSword = starterWeapon.item.copy(damage = 25)
+
+    println("Weapon setelah upgrade: $upgradedStarterSword")
+
+    println("")
+
+    // Simulasi
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedStarterSword))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
