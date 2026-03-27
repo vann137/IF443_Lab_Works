24f90a5b8625b59400798111010f3271ee8b0be7
VannShiki <sanjayaarie858@gmail.com>
2026-03-27 11:11:54 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
