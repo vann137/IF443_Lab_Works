fc96611bcb6934d36afe9a469d200d9d872caff3
VannShiki <sanjayaarie858@gmail.com>
2026-04-17 15:43:50 +0700

    week08: (task) implement payment processing with !!

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week08/ApiParser.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week08/ApiParser.kt
@@ -26,5 +26,17 @@ class ApiParser {
 
             else -> null
         }
+
+    }
+
+    fun checkout(product: Product) {
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        val trxId = JavaPaymentService.processPayment(id)!!
+        println("Transaction Success: $trxId")
+
     }
 }
