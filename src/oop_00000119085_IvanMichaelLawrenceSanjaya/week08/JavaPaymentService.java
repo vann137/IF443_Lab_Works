6a7deff4794baa0d4aa5abae663e3ba47a9dc576
VannShiki <sanjayaarie858@gmail.com>
2026-04-17 15:42:00 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
