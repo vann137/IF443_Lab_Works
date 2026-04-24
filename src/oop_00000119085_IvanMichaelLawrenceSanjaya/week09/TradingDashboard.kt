b0cb9ff43157114e7edb3e41636efccfee2b57e6
VannShiki <sanjayaarie858@gmail.com>
2026-04-24 13:20:25 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week09/TradingDashboard.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week09/TradingDashboard.kt
@@ -29,4 +29,8 @@ fun main(){
         .sortedBy { it.roe }
         .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
+    // Pipeline Tambahan: Ekstraksi Unik (Set)
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .toSet()
 }
