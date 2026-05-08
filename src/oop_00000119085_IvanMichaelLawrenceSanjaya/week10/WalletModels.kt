bde89a080fd81b2343ae2e837ee79de4a1079537
VannShiki <sanjayaarie858@gmail.com>
2026-05-08 08:13:54 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/WalletModels.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/WalletModels.kt
@@ -1,4 +1,8 @@
 package oop_00000119085_IvanMichaelLawrenceSanjaya.week10
 
-data class Coin(val name: String, val balance: Double)
+interface INamedEntity {
+    val name: String
+}
+
+data class Coin(override val name: String, val balance: Double): INamedEntity
 data class Transaction(val id: String, val amount: Double)
