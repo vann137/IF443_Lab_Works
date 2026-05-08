344dedc3a5a062638b2f545d13c9b14e3f7a0989
VannShiki <sanjayaarie858@gmail.com>
2026-05-08 08:01:28 +0700

    week10: implement generic function with return type

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/GenericMethods.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000119085_IvanMichaelLawrenceSanjaya.week10
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T { // [cite: 60]
+    return input // [cite: 60]
+}
