331f23a97bf52b88d7e5c9e5aabda8ae1089f21e
VannShiki <sanjayaarie858@gmail.com>
2026-05-08 11:05:43 +0700

    week11: create nullable receiver extension

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week11/StringExtensions.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week11/StringExtensions.kt
@@ -8,3 +8,9 @@ fun String.addGreeting(): String {
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
 }
+
+// Extension function dengan nullable receiver
+fun String?.isNullOrEmptyCustom(): Boolean {
+    // 'this' bisa bernilai null, jadi harus ditangani
+    return this == null || this.isEmpty()
+}
