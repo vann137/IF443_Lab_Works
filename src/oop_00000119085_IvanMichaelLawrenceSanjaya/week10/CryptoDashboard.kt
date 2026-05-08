da165693dad385662d2fe9e9df7f476318f47fb4
VannShiki <sanjayaarie858@gmail.com>
2026-05-08 08:22:56 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/CryptoDashboard.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main(){
     coinRepo.add(Coin("BTC", 0.43))
     coinRepo.add(Coin("ETH", 5.17))
     coinRepo.add(Coin("USDT", 2350.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
