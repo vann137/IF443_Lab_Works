ebb6de725e6fb3c9656ba93943fd47bdfe6868b4
VannShiki <sanjayaarie858@gmail.com>
2026-05-15 10:55:11 +0700

    week12: (task) setup SmartFeeder main function

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week12/SmartFeeder.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week12/SmartFeeder.kt
@@ -13,3 +13,7 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
+
+fun main(){
+    var currentKibbleStock = 50
+}
