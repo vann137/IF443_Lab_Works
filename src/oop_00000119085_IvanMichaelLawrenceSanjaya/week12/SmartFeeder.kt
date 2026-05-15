3a29678c1e21d6d626af72ecd468b9229197ca52
VannShiki <sanjayaarie858@gmail.com>
2026-05-15 10:58:20 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week12/SmartFeeder.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week12/SmartFeeder.kt
@@ -29,4 +29,8 @@ fun main(){
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n=== Jadwal Makan 2 ===")
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
