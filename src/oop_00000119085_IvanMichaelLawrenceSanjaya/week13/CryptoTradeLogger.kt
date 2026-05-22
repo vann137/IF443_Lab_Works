892d4e2c6fb16ef1b7635fb22910eb508ddd1cd7
VannShiki <sanjayaarie858@gmail.com>
2026-05-22 15:06:00 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week13/CryptoTradeLogger.kt
@@ -0,0 +1,5 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
