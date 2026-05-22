bfca3b8647afa5123a1d294553815e06fd225642
VannShiki <sanjayaarie858@gmail.com>
2026-05-22 15:08:10 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_00000119085_IvanMichaelLawrenceSanjaya/week13/CryptoTradeLogger.kt
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week13/CryptoTradeLogger.kt
@@ -5,3 +5,19 @@ import java.io.FileNotFoundException
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
+
+fun fromCsvTrade(line: String): TradeRecord? {
+    return try {
+        val parts = line.split(",")
+        TradeRecord(
+            id = parts[0].trim().toInt(),
+            symbol = parts[1].trim(),
+            type = parts[2].trim(),
+            margin = parts[3].trim().toDouble(),
+            pnl = parts[4].trim().toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
+}
