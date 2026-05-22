c55e8ea7d466ac92792637e6c3830c91f8c1646e
VannShiki <sanjayaarie858@gmail.com>
2026-05-22 14:59:57 +0700

    week13: create Student model

---
+++ src/oop_00000119085_IvanMichaelLawrenceSanjaya/week13/StudentSerialization.kt
@@ -0,0 +1,5 @@
+package oop_00000119085_IvanMichaelLawrenceSanjaya.week13
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(val name: String, val age: Int, val gpa: Double)
